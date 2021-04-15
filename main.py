2ed2ea8db9708488c42a9a5b22cd3038019b725b
octo.jon <stapleton.jon.michael@gmail.com>
2021-04-15 14:48:23 +0000

    added "pick up" command

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from adventurelib import *
 def look():
   print("you look around")
 
+@when("pick up a thing")
+def look():
+  print("you pick it up")
+
 @when("stand up")
 def look():
   print("you stand up")
