83bfd255e6a623f2ef40d918d7e151e61ac58471
Mahki Burroughs <>
2021-04-15 15:05:57 +0000

    fixed the defining variable in "stand up" from def look(): to def stand_up(): also added the "walk around" feature

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ def look():
   print("you look around")
 
 @when("stand up")
-def look():
+def stand_up():
   print("you stand up")
+
+@when("walk around")
+def walk_around():
+  print("you walk around looking for something")  
 
 start()
 
