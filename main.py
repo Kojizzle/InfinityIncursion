a49ddd5f4f5166842d8401f51907943020f990aa
Mahki Burroughs <>
2021-04-20 14:59:24 +0000

    added elements to the horror scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from adventurelib import *
 
+
 @when("look around")
 def look():
   print("you look around")
@@ -10,11 +11,16 @@ def stand_up():
 
 @when("walk around")
 def walk_around():
-  print("you walk around looking for something")  
+  print("you walk around looking for something and you notice yourself in a strange other-worldly dimension. you look up and see other planets very close and 4 red suns")  
 
 @when("realize")
 def realize():
-  print("you realize you are in a dream you can control")  
+  print("you realize you are in a dream you can control")
+
+
+@when("look at the mirror")
+def mirrored_horror():
+  print("you look into the mirror and see a twisted and contorted version of yourself that sends chills down your spine then it fades away you have a choice to panic and run or keep walking calmly in this bizarre world")  
 
 
 
