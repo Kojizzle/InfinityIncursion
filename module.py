f3b2267e4131264c19b960f2cb1c8d0ac0244ab5
octo.jon <stapleton.jon.michael@gmail.com>
2021-04-22 14:33:17 +0000

    Added "look around" command

--- module.py
+++ module.py
@@ -1,6 +1,10 @@
 from adventurelib import *
 
 
-@when("walk around", context="outside")
+@when("walk around", context="forest")
 def walk():
-  print("you stub your toe")
+  print("you stub your toe on a log.")
+
+@when("look around", context="forest")
+def explore():
+  print("You see a dark forest, filled with the sounds of wildlife.")
