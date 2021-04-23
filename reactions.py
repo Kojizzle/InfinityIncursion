778941cd2aa1c04c2c740f1fe41b584be0ff6787
Mahki Burroughs <>
2021-04-23 14:40:34 +0000

    fixed some stuff

--- reactions.py
+++ reactions.py
@@ -1,9 +1,9 @@
 from adventurelib import *
 
-@when("run away")
+@when("run away", context="decision1")
 def you_run_into_the_unknown():
   print("you quickly realize running is useless as all you see are more horrifying things around you until you faint and die. wake up wake up your dreaming")
 
-@when("walk calmly")
+@when("walk calmly", context="decision1")
 def you_change_the_environment_around_you():
   print("as you convince yourself everything will be ok you notice the atmosphere around you changing to a more happy tone with good things happening around you it feels like time is flying as you have fun playing games at a carnival while also making friends. while you are blindly having fun a figure resembling a clown stands by the tree-line")
