5702b9154ef827b24250059d1d5ec8d4d0e86868
Divyesh Sharma <>
2021-04-26 17:32:44 +0000

    Completed q1 and 2, set base for rest of code

--- popquiz.py
+++ popquiz.py
@@ -4,8 +4,8 @@ def restart():
   print("Welcome to the school pop quiz! type in 'begin' to begin, and type in 'hint' when you need a hint on a certain question. There are four questions. Good luck!")
 
 print(restart())
-
-answers= ["6.022x10^23", "4", "q3", "q4"]
+questions= ["q1","q2","q3","q4"]
+answers= ["6.022x10^23", "4", "a3", "a4"]
 hint= ["What are the amount of atoms in a mole?", "Use addition", "q3h", "q4h"]
 current_q=0
 
@@ -15,7 +15,7 @@ def restart():
 
 @when("begin")
 def q1():
-  print("What is the numerical factor of Avogadro's number?")
+  print(questions[current_q])
   set_context("question")
 
 @when("hint", context= "question")
@@ -27,23 +27,16 @@ def give_hint():
 def give_ans1(answer):
   global current_q
   if answer == answers[current_q]:
-    print("That is correct! Type 'question two' to go to the second question")
-    current_q + 1
+    print("That is correct! Type 'next question' to go to the next question")
+    current_q = current_q+1
     set_context("answered")
   else:
     print("That is incorrect! type in 'restart' to retake the quiz.")
-@when("question two")
+@when("next question")
 def q2():
-  print("what is 2+2?")
+  print(questions[current_q])
   set_context("question")
 
-@when("ANSWER", context="question")
-def give_ans2(answer):
-  global current_q
-  if answer == answers[current_q]:
-    print("That is correct! Type 'q3' to go to the third question")
-    current_q + 1
-    set_context("answered")
 
 
 
