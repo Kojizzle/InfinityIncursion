02bf8fea39658be9a10064b46a9e592dc507093f
Divyesh Sharma <>
2021-04-26 15:25:13 +0000

    Created question 2, error with current_q not updating

--- popquiz.py
+++ popquiz.py
@@ -5,8 +5,8 @@ def restart():
 
 print(restart())
 
-answers= ["6.022x10^23", "q2", "q3", "q4"]
-hint= ["What are the amount of atoms in a mole?", "q2h", "q3h", "q4h"]
+answers= ["6.022x10^23", "4", "q3", "q4"]
+hint= ["What are the amount of atoms in a mole?", "Use addition", "q3h", "q4h"]
 current_q=0
 
 @when("restart")
@@ -27,11 +27,24 @@ def give_hint():
 def give_ans1(answer):
   global current_q
   if answer == answers[current_q]:
-    print("That is correct! Type 'q2' to go to the second question")
+    print("That is correct! Type 'question two' to go to the second question")
     current_q + 1
     set_context("answered")
   else:
     print("That is incorrect! type in 'restart' to retake the quiz.")
+@when("question two")
+def q2():
+  print("what is 2+2?")
+  set_context("question")
+
+@when("ANSWER", context="question")
+def give_ans2(answer):
+  global current_q
+  if answer == answers[current_q]:
+    print("That is correct! Type 'q3' to go to the third question")
+    current_q + 1
+    set_context("answered")
+
 
 
 
