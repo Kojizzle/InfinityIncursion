0945e33ec8104f34a8789a1a3c9821d3a6a2d611
pvlevc59 <pvlevc59@harrisonburg.k12.va.us>
2021-04-27 14:18:35 -0400

    Attempt at making items work

--- room.py
+++ room.py
@@ -3,24 +3,47 @@ from adventurelib import *
 name=str(input("enter your name: "))
 
 
+room1 = Room("""You awake in a poorly lit and dark room. 
 
-print("""You awake in a dark, poorly lit and dark room. 
 The only light is coming from the main door. An uneasy feeling has you overtaken. 
-You are determined to get out of here. """)
+You are determined to get out of here.""")
+
+hallway = Room("You walk through the door into a long hallay, with a series of doors on each side of the wall.")
+
+current_room = room1
 
 
-@when("look around")
+
+set_context("darkroom")
+
+crowbar = Item('crowbar')
+
+current_room.items = crowbar
+
+
+
+
+@when("look around", context='darkroom')
 def look():
   print("The room is dark, and the only light is coming from the main door. You manage to notice a crowbar laying the floor")
 
-@when("stand up")
+@when("stand up", context='darkroom')
 def stand_up():
   print("you stand up, although when you check what you were sitting on, nothing seems to be there.")
 
-@when("inspect")
+@when("inspect", context="darkroom")
 def inspect():
   print("You inspect the walls closer, the color is black and rusty, strange material.") 
   
 
+  @when("take crowbar", context="darkroom")
+  def take(item):
+    obj = current_room.items.take(item)
+    if not obj:
+      print(f"You already took the crowbar.")
+    else:
+      print(f"You took the {crowbar}.")
+      inventory.add(obj)
+    
 
 
