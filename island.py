804bd7e7e7678f637b30cdd5d5331ab246b16e8b
Chase Baker <>
2021-04-30 15:19:25 +0000

    Adding more to the story, also fixed a small issue.

--- island.py
+++ island.py
@@ -61,15 +61,37 @@ def look_around_shoreline():
   print("""You look around and see the cave you just ran from not too far from you. You also see a mountain that was wasn't visible beforehand due to the fog. You see a pathway leading to the top that you can use to climb the mountain.""")
 
 @when("climb the mountain", context='shoreline2')
-set_context('mountain_pathway')
 def climb_mountain():
+  set_context('mountainpathway')
   print("""You start to climb the mountain, but halfway up the pathway when you hear loud cracking up above you. You look up to see a big boulder hurtiling towards you, but you can't tell where it's exactly going to land. You can either dodge forward or dodge backwards.""")
 
-  @when("dodge forward", context='mountain_pathway')
+@when("dodge forward", context='mountainpathway')
 def dodge_forward():
   set_context('death')
   print("""You dodged the wrong way and were crushed by the boulder.""")
 
-  @when("dodge backward", context='mountain_pathway')
+@when("dodge backward", context='mountainpathway')
 def dodge_backward():
+  set_context('blocked')
   print("""You just managed to dodge the boulder as it lands with a big crack in front of you, but your pathway is now blocked.""")
+
+@when("walk down the mountain", context='blocked')
+def down_mountain():
+  set_context('shoreline3')
+  print("""You walk down the mountain back down to the shoreline.""")
+
+@when("stand up", context='shoreline3')
+def stand_up_shoreline():
+  print("""You stand up, causing the sand to shift beneath your feet.""")
+
+@when("sit down", context='shoreline3')
+def sit_down_shoreline():
+  print("""You sit down on the soft sand beneath you.""")
+
+@when("look around", context='shoreline3')
+def look_around():
+  print("""You look around at the environment surrounding you, but you feel hungry an thirsty. You notice a nearby tree with some fruit hanging off of it.""")
+
+@when("walk to tree", context='shoreline3')
+def walk_tree():
+  print("""You walk over to the tree to identify that the fruit's on the tree are bananas.""")
